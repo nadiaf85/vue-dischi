3de3d514d06839fb534222102c65cc6569e4812a
nadiaf85 <nadia.fiorio@alice.it>
2022-02-03 20:29:01 +0100

    modifiche css

--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -23,8 +23,9 @@ export default {
 <style lang="scss" scoped>
 
 main{
-    background-color: #2c2c50;
+    background-color: #1e2d3b;
     padding: 60px;
+    min-height: calc(100vh - 60px)
 }
 
 </style>
